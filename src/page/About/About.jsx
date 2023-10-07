79bc2710f11a1ac94ee1387fedac2da7d4f22b8f
md moniruzzaman arafat <arafatmonir091@gmail.com>
2023-10-08 01:24:02 +0600

    about us

--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -2,7 +2,17 @@
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <div className="flex items-center gap-5">
+                <div className="w-1/2 space-y-5">
+                    <h1 className="text-4xl font-bold">About Educenter</h1>
+                    <p className="text-justify">In a world where education knows no boundaries, [Online Education System Name] stands as a beacon of e-learning excellence. Established in [Year], our platform has bridged distances, transcending traditional classroom walls to bring quality education right to the fingertips of learners worldwide.
+
+                        Our mission is simple: To democratize education, making it accessible, flexible, and tailored to the modern needs. Leveraging cutting-edge technology, we provide a comprehensive range of courses curated and delivered by a cadre of esteemed educators and professionals from across the globe.</p>
+                </div>
+                <div className="w-1/2">
+                    <img src="https://i.ibb.co/kJQrr9Y/undraw-Teacher-re-sico.png" className="w-full" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,9 +1,13 @@
 import Slider from "../../component/Slider/Slider";
+import About from "../About/About";
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <div className="w-10/12 mx-auto">
+                <About></About>
+            </div>
         </div>
     );
 };
